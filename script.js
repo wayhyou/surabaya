06dbe39e8cf946b1b8cc24f9aa536058d053599a
WAHYU EKO LAKSONO <45890489+wayhyou@users.noreply.github.com>
2024-04-12 20:30:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Navbar 
+const navbar = document.getElementById('navbar');
+    let lastScrollTop = 0;
+
+    window.addEventListener('scroll', function() {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+      if (scrollTop > lastScrollTop && scrollTop > 90) {
+        // Scrolling down
+        navbar.classList.add('hidden-nav');
+      } else {
+        // Scrolling up or at the top
+        navbar.classList.remove('hidden-nav');
+      }
+
+      lastScrollTop = scrollTop;
+    });
